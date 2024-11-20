5217b496d77997f02947b3bda03e9ca1ad0600ab
Mungunsolongo <mungunsolongo.batbayar@gmail.com>
2024-11-20 13:16:15 +0800

    test

--- lab8.html/lab8.js
+++
@@ -1,9 +0,0 @@
-1. Гараас өгсөн тооны цифрүүдийн нийлбэр нь палимдром мөн эсэхийг шалгадаг функц
-String originalStr = "Hello";
-String reversedStr = "";
-
-for (int i = 0; i < originalStr.length(); i++) {
-  reversedStr = originalStr.charAt(i) + reversedStr;
-}
-
-System.out.println("Reversed string: "+ reversedStr);
